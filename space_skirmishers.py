54ead1d73d227b0d41e218947e1f662986c1c319
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2017-03-15 19:51:51 -0400

    Improvements to codebase and better handling of collisions
    Added a lot of comments for readability
    Tweaked speed values again

--- space_skirmishers.py
+++ space_skirmishers.py
@@ -37,17 +37,17 @@ TITLE_TEXT_SIZE = 75
 SUBTITLE_TEXT_SIZE = 40
 TITLE_SUBTITLE_MARGIN = 10
 
-PLAYER_WIDTH = 20
-PLAYER_HEIGHT = 60
-PLAYER_SPEED = 30
-PLAYER_1_BULLET_VECTOR_X = 1
-PLAYER_2_BULLET_VECTOR_X = -1
-PLAYER_MAX_BULLETS = 1
+PLAYER_WIDTH = 25
+PLAYER_HEIGHT = 80
+PLAYER_SPEED = 20
+PLAYER_1_BULLET_DIRECTION_X = 1
+PLAYER_2_BULLET_DIRECTION_X = -1
+PLAYER_MAX_BULLETS = 2
 
 BULLET_WIDTH = 10
 BULLET_HEIGHT = 5
 BULLET_DELAY = 500
-BULLET_SPEED = 25
+BULLET_SPEED = 20
 
 OBSTACLE_HEIGHT = 20
 OBSTACLE_WIDTH = 20
@@ -142,7 +142,7 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((self.width, self.height))
         self.rect = self.image.get_rect()
         self.speed = PLAYER_SPEED
-        self.vectory = 0
+        self.direction_y = 0
         self.name = name
         self.key_move_up = key_move_up
         self.key_move_down = key_move_down
@@ -160,10 +160,10 @@ class Player(pygame.sprite.Sprite):
     def check_bounds(self):
         if self.rect.top < BOUNDS_MIN_Y:
             self.rect.top = BOUNDS_MIN_Y
-            self.vectory = 0
+            self.direction_y = 0
         elif self.rect.bottom > BOUNDS_MAX_Y:
             self.rect.bottom = BOUNDS_MAX_Y
-            self.vectory = 0
+            self.direction_y = 0
 
     def compute_hit(self):
         if self.hit_points > 0:
@@ -176,7 +176,7 @@ class Player(pygame.sprite.Sprite):
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, rect, color, vectorx, speed):
+    def __init__(self, rect, color, direction_x, speed):
         pygame.sprite.Sprite.__init__(self)
         self.width = BULLET_WIDTH
         self.height = BULLET_HEIGHT
@@ -187,13 +187,13 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.centery = rect.centery
         self.rect.left = rect.left + BULLET_MARGIN
         self.name = BULLET_NAME
-        self.vectorx = vectorx
+        self.direction_x = direction_x
         self.speed = speed
 
     def update(self, *args):
-        self.rect.x += self.vectorx * self.speed
+        self.rect.x += self.direction_x * self.speed
 
-        if self.rect.left < BOUNDS_MIN_X or self.rect.right > BOUNDS_MAX_X:
+        if self.rect.left < 0 or self.rect.right > SCREEN_WIDTH:
             self.kill()
 
 
@@ -231,6 +231,7 @@ class Game:
     def __init__(self):
         pygame.init()
         self.display_screen, self.display_rect = self.make_screen()
+        self.intro_sound = pygame.mixer.Sound('assets/intro.wav')
         self.laser_sound = pygame.mixer.Sound('assets/laser.ogg')
         self.block_kill_sound = pygame.mixer.Sound('assets/block_kill.wav')
         self.player_hit_sounds = [
@@ -238,12 +239,13 @@ class Game:
             pygame.mixer.Sound('assets/player_hit_1_hp.wav'),
             pygame.mixer.Sound('assets/player_hit_2_hp.wav')
         ]
-        self.intro_sound = pygame.mixer.Sound('assets/intro.wav')
-        self.game_over = False
-        self.game_to_start = True
-        self.game_started = False
+        # Initialize flags to new game state
+        self.game_over_state = False
+        self.new_game_state = True
+        self.game_started_state = False
 
     def make_screen(self):
+        # Create screen and its rectangle for display
         pygame.display.set_caption(GAME_TITLE)
         display_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         display_rect = display_screen.get_rect()
@@ -252,12 +254,12 @@ class Game:
         return display_screen, display_rect
 
     def make_players(self):
+        # Create players
         player_1 = Player(PLAYER_1_NAME, PLAYER_1_KEY_UP, PLAYER_1_KEY_DOWN)
         player_2 = Player(PLAYER_2_NAME, PLAYER_2_KEY_UP, PLAYER_2_KEY_DOWN)
-
+        # Position them on their initial positions
         player_1.rect.top = BOUNDS_MIN_Y
         player_1.rect.left = BOUNDS_MIN_X
-
         player_2.rect.bottom = BOUNDS_MAX_Y
         player_2.rect.right = BOUNDS_MAX_X
 
@@ -265,46 +267,40 @@ class Game:
 
     def make_obstacles(self):
         obstacle_group = pygame.sprite.Group()
-
+        # Load one of the levels and its obstacles
         level = LEVELS[randint(0, 1)]
-
         for obstacle in level:
             obstacle_group.add(self.make_obstace_group(obstacle["top"], obstacle["left"], obstacle["rows"], obstacle["columns"]))
-
         return obstacle_group
 
     def make_obstace_group(self, top, left, rows, columns):
         obstacle_group = pygame.sprite.Group()
-
         for row in range(rows):
             for column in range(columns):
                 x = left + column * OBSTACLE_WIDTH
                 y = top + row * OBSTACLE_HEIGHT
                 obstacle = Obstacle(x, y, COLOR_OBSTACLE)
                 obstacle_group.add(obstacle)
-
         return obstacle_group
 
     def check_input(self):
         for event in pygame.event.get():
+            # Update value of keys to pass it along to sprites later
             self.keys = pygame.key.get_pressed()
-
             if event.type == pygame.QUIT:
                 self.quit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == PLAYER_1_KEY_SHOOT and len(self.player_1_bullets) < PLAYER_MAX_BULLETS:
                     # Pew pew
                     self.laser_sound.play()
-
-                    bullet = Bullet(self.player_1.rect, COLOR_PLAYER_1_BULLET, PLAYER_1_BULLET_VECTOR_X, BULLET_SPEED)
+                    # Add bullet to sprite groups with the correct speed and direction
+                    bullet = Bullet(self.player_1.rect, COLOR_PLAYER_1_BULLET, PLAYER_1_BULLET_DIRECTION_X, BULLET_SPEED)
                     self.player_1_bullets.add(bullet)
                     self.all_bullets.add(bullet)
                     self.all_sprites.add(bullet)
                 elif event.key == PLAYER_2_KEY_SHOOT and len(self.player_2_bullets) < PLAYER_MAX_BULLETS:
-                    # Pew pew
                     self.laser_sound.play()
-
-                    bullet = Bullet(self.player_2.rect, COLOR_PLAYER_2_BULLET, PLAYER_2_BULLET_VECTOR_X, BULLET_SPEED)
+                    bullet = Bullet(self.player_2.rect, COLOR_PLAYER_2_BULLET, PLAYER_2_BULLET_DIRECTION_X, BULLET_SPEED)
                     self.player_2_bullets.add(bullet)
                     self.all_bullets.add(bullet)
                     self.all_sprites.add(bullet)
@@ -314,34 +310,48 @@ class Game:
             if event.type == pygame.QUIT:
                 self.quit()
             elif event.type == pygame.KEYUP:
-                self.game_to_start = True
-                self.game_started = False
-                self.game_over = False
+                # Update flags to new game state
+                self.new_game_state = True
+                self.game_started_state = False
+                self.game_over_state = False
 
     def check_game_to_start_input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.quit()
             elif event.type == pygame.KEYUP:
-                self.game_to_start = False
-                self.game_started = True
-                self.game_over = False
+                # Update flags to game started state
+                self.new_game_state = False
+                self.game_started_state = True
+                self.game_over_state = False
                 self.intro_sound.play()
 
     def check_collisions(self):
-        hits = pygame.sprite.groupcollide(self.all_bullets, self.all_obstacles, True, False)
-        if hits:
-            self.block_kill_sound.play()
-            for obstacles in hits.values():
-                obstacles[0].kill()
-
-
+        hits_bullets_1 = pygame.sprite.groupcollide(self.player_1_bullets, self.all_obstacles, True, False)
+        hits_bullets_2 = pygame.sprite.groupcollide(self.player_2_bullets, self.all_obstacles, True, False)
+        # Check collisions between each player's bullets and obstacles
+        for bullet, obstacles in hits_bullets_1.items():
+            closest = obstacles[0]
+            for obstacle in obstacles:
+                # Get the topmost and leftmost obstacle to remove
+                if obstacle.rect.left < closest.rect.left and obstacle.rect.top < closest.rect.top:
+                    closest = obstacle
+            closest.kill()
+        for bullet, obstacles in hits_bullets_2.items():
+            closest = obstacles[0]
+            for obstacle in obstacles:
+                # Gett the topmost and rightmost obstacle to remove
+                if obstacle.rect.left > closest.rect.left and obstacle.rect.top < closest.rect.top:
+                    closest = obstacle
+            closest.kill()
+        # Check collisions between bullets and players
         for bullet in self.player_1_bullets:
             if pygame.sprite.collide_rect(bullet, self.player_2):
+                # Handle each hit's effect on player and bullet
                 self.player_2.compute_hit()
+                # Play relevant sound effect (according to player HP)
                 self.player_hit_sounds[self.player_2.hit_points].play()
                 bullet.kill()
-
         for bullet in self.player_2_bullets:
             if pygame.sprite.collide_rect(bullet, self.player_1):
                 self.player_1.compute_hit()
@@ -350,10 +360,10 @@ class Game:
 
     def check_game_over(self):
         if self.player_1.hit_points == 0 or self.player_2.hit_points == 0:
-            self.game_over = True
-            self.game_to_start = False
-            self.game_started = False
-
+            # Update flags to game over state
+            self.game_over_state = True
+            self.new_game_state = False
+            self.game_started_state = False
             self.handle_game_over()
 
     def handle_game_over(self):
@@ -364,7 +374,7 @@ class Game:
             subtitle = "{} wins".format(self.player_1.name)
         else:
             subtitle = "{} wins".format(self.player_2.name)
-
+        # Update game over subtitle with game ending message (according to winners/losers)
         self.game_over_subtitle = Text(
             'assets/arcadeclassic.ttf',
             SUBTITLE_TEXT_SIZE,
@@ -379,6 +389,7 @@ class Game:
         sys.exit()
 
     def setup_game(self):
+        # Set up text elements
         self.intro_title = Text(
             'assets/arcadeclassic.ttf',
             TITLE_TEXT_SIZE,
@@ -401,8 +412,7 @@ class Game:
             WHITE,
             self.display_rect
         )
-
-        # Setting up sprites
+        # Set up sprites
         self.player_1, self.player_2 = self.make_players()
         self.player_1_bullets = pygame.sprite.Group()
         self.player_2_bullets = pygame.sprite.Group()
@@ -410,53 +420,52 @@ class Game:
         self.obstacle_group_1 = self.make_obstacles()
         self.all_obstacles = pygame.sprite.Group(self.obstacle_group_1)
         self.all_sprites = pygame.sprite.Group(self.player_1, self.player_2, self.all_obstacles, self.all_bullets)
-
-        # Setting up timing and inputs
+        # Set up timing and inputs
         self.fps = 30
         self.keys = pygame.key.get_pressed()
         self.clock = pygame.time.Clock()
-
-        self.game_to_start = True
-        self.game_started = False
-        self.game_over = False
+        # Set up flags
+        self.new_game_state = True
+        self.game_started_state = False
+        self.game_over_state = False
 
     def main_loop(self):
         while True:
-            if self.game_to_start:
+            if self.new_game_state:
                 self.setup_game()
-                self.game_over = False
-
+                self.game_over_state = False
+                # Handle events
+                self.check_game_to_start_input()
+                # Draw surface
                 self.display_screen.fill(COLOR_BACKGROUND)
-
                 self.intro_title.draw(self.display_screen)
                 self.intro_subtitle.draw(self.display_screen)
-
-                self.check_game_to_start_input()
+                # Show surface
                 pygame.display.update()
-
-            elif self.game_over:
+            elif self.game_over_state:
+                # Handle events
+                self.check_game_over_input()
+                # Draw surface
                 self.display_screen.fill(COLOR_BACKGROUND)
-
                 self.game_over_title.draw(self.display_screen)
                 self.game_over_subtitle.draw(self.display_screen)
-
-                self.check_game_over_input()
+                # Show surface
                 pygame.display.update()
-
-            elif self.game_started:
+            elif self.game_started_state:
                 current_time = pygame.time.get_ticks()
-                self.display_screen.fill(COLOR_BACKGROUND)
-
-                # Handling inputs and updates
+                # Handle events
                 self.check_input()
+                self.check_game_over()
+                # Draw surface
+                self.display_screen.fill(COLOR_BACKGROUND)
+                # Update game elements
                 self.all_sprites.update(self.keys, current_time)
                 self.check_collisions()
-
-                # Drawing game
+                # Drawing surface
                 self.all_sprites.draw(self.display_screen)
+                # Show surface
                 pygame.display.update()
 
-                self.check_game_over()
                 self.clock.tick(self.fps)
 
 
